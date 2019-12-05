77e4af43a8f39995bcb33b5946f47809c40377bb
whiz25 <afanimartin@gmail.com>
2019-12-05 13:42:17 +0300

    Implement BooksList component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import BooksList from './containers/booksList';
+import BooksForm from './containers/booksForm';
 
 function App() {
-  return <div className="app">app</div>;
+  return (
+    <div className="app">
+      <BooksForm />
+      <BooksList />
+    </div>
+  );
 }
 
 export default App;
--- src/containers/booksList.js
+++ src/containers/booksList.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
 
 const BooksList = () => {
-  return <p>Test Code</p>;
+  return (
+    <table className="books-list">
+      <tr>
+        <th>Book ID</th>
+        <th>title</th>
+        <th>category</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>code in react</td>
+        <td>programming</td>
+      </tr>
+    </table>
+  );
 };
 
 export default BooksList;
