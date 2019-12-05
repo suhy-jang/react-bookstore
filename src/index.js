25df4a9509e1d48d851cf53d608489172eaa8134
shjang7 <lucy.sh.jang@gmail.com>
2019-12-05 18:52:55 +0900

    Add eslint, prettier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/normalize.css';
+import './assets/style.css';
+import App from './app';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
