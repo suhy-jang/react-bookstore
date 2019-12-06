fc494f8fe5219dd397e40432be873ebe6d5bcb74
whiz25 <afanimartin@gmail.com>
2019-12-06 13:42:43 +0300

    Implement CHANGE_FILTER action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,5 @@
 export const createBook = book => ({ type: 'CREATE_BOOK', book });
 export const removeBook = book => ({ type: 'REMOVE_BOOK', book });
+
+
+export const changeFilter = filter => ({ type: 'CHANGE_FILTER', filter });
